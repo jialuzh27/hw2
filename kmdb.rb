90de64260145b981e48bd69a140559b9a7a56e31
jialuzh27 <jialu.zhou@kellogg.northwestern.edu>
2022-02-08 04:55:47 +0000

    update

--- kmdb.rb
+++ kmdb.rb
@@ -212,6 +212,12 @@ puts ""
 # Query the movies data and loop through the results to display the movies output
 # TODO!
 
+movies = Movie.all
+for movie in movies
+    director = People.where({id: movie.person_id}) [0]
+    puts "#{movie.title} #{movie.year_released} #{movie.rated} #{director.name}"
+end
+
 # Prints a header for the cast output
 puts ""
 puts "Top Cast"
@@ -220,3 +226,10 @@ puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie
 # TODO!
+roles=Role.all
+
+for role in roles
+    movie = Movie.where({id: role.movie_id}) [0]
+    person = People.where({id: role.person_id}) [0]
+    puts "#{movie.title} #{person.name} #{role.character_name}" 
+end
